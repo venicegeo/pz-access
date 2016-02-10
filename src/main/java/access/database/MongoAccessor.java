7666aaf4bd315c4b9ac6a0cbfe1800bd71b11cca
GitHubRGI <patrick.doody@rgi-corp.com>
2016-02-10 13:20:16 -0500

    Add license text

--- src/main/java/access/database/MongoAccessor.java
+++ src/main/java/access/database/MongoAccessor.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016, RadiantBlue Technologies, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package access.database;
 
 import java.net.UnknownHostException;
@@ -18,6 +33,7 @@ import access.database.model.Lease;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
 import com.mongodb.MongoTimeoutException;
 
 /**
@@ -113,7 +129,7 @@ public class MongoAccessor {
 				throw new ResourceAccessException("Data not found.");
 			}
 		} catch (MongoTimeoutException mte) {
-			throw new ResourceAccessException("MongoDB instance not available.");
+			throw new MongoException("MongoDB instance not available.");
 		}
 
 		return data;
