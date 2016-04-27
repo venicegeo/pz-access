b8fea6873bdf22eb2740300d4db7555b7656ab9e
go0ty <patrick.doody@rgi-corp.com>
2016-04-27 08:45:17 -0400

    grab vcap postgis from geoserver service

--- src/main/java/access/controller/AccessController.java
+++ src/main/java/access/controller/AccessController.java
@@ -80,15 +80,15 @@ import access.messaging.AccessThreadManager;
 @RestController
 public class AccessController {
 
-	@Value("${vcap.services.pz-postgres.credentials.host}")
+	@Value("${vcap.services.pz-geoserver.postgres.host}")
 	private String POSTGRES_HOST;
-	@Value("${vcap.services.pz-postgres.credentials.port}")
+	@Value("${vcap.services.pz-geoserver.postgres.port}")
 	private String POSTGRES_PORT;
-	@Value("${vcap.services.pz-postgres.credentials.database}")
+	@Value("${vcap.services.pz-geoserver.postgres.database}")
 	private String POSTGRES_DB_NAME;
-	@Value("${vcap.services.pz-postgres.credentials.username}")
+	@Value("${vcap.services.pz-geoserver.postgres.username}")
 	private String POSTGRES_USER;
-	@Value("${vcap.services.pz-postgres.credentials.password}")
+	@Value("${vcap.services.pz-geoserver.postgres.password}")
 	private String POSTGRES_PASSWORD;
 	@Value("${postgres.schema}")
 	private String POSTGRES_SCHEMA;
